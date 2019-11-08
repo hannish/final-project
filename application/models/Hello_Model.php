d0d7e362b800eac80599f5730ec9597c45bf3ece
hannish <hannish04@gmail.com>
2019-11-08 17:44:40 +0000

    code update for UPDATE API call

--- application/models/Hello_Model.php
+++ application/models/Hello_Model.php
@@ -18,6 +18,12 @@ class Hello_Model extends CI_Model
 	$this->db->query("delete  from catalog_new_v1 where item_id='".$id."'");
 	}
 	
+	function displayrecordsById($id)
+	{
+	$query=$this->db->query("select * from catalog_new_v1 where item_id='".$id."'");
+	return $query->result();
+	}
+
 	function updaterecords($servername,$catagory,$serversize,$uptime,$id)
 	{
 	$query=$this->db->query("update catalog_new_v1 SET servername='$servername',catagory='$catagory',serversize='$serversize',uptime=$uptime where item_id='".$id."'");
